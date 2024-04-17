61267ba9ae4ebf2f0109b2e217c043fe3fa873b0
Marco Antônio <ma.souza.junior@gmail.com>
2024-04-17 09:47:41 -0300

    fea: add module docs

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+/**
+ * @module
+ *
+ * This module contains a function to check if you are acting first and thinking later
+ *
+ *    *
+ * ```ts
+ * import { act } from "jsr:@act/act";
+ *
+ * act("act first, think later") // true
+ * act("think first, act later") // false
+ * act("don't act") // false
+ * ```
+ */
+
 export function act(action: string): boolean {
   const actIndex = action.toLowerCase().indexOf("act");
   const thinkIndex = action.toLowerCase().indexOf("think");
