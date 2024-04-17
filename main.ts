b9ec2a8c90c379ecfeafaabe21630cefc13ca1c6
Marco Antônio <ma.souza.junior@gmail.com>
2024-04-17 10:03:26 -0300

    enh: improve function documentation

--- main.ts
+++ main.ts
@@ -13,6 +13,17 @@
  * ```
  */
 
+/**
+ * This function receives a text and literally check if you are act first and think later
+ *
+ * ```ts
+ * import { act } from "jsr:@act/act";
+ *
+ * act("act first, think later") // true
+ * act("think first, act later") // false
+ * act("don't act") // false
+ * ````
+ */
 export function act(action: string): boolean {
   const actIndex = action.toLowerCase().indexOf("act");
   const thinkIndex = action.toLowerCase().indexOf("think");
